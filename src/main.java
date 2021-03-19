610d5460ad9447d160c44bcde0fc46740f20c6f1
Yulia <seminajlj@rambler.ru>
2021-03-19 21:11:30 +0300

    Первый коммит

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+ int balans = 2000000000
+         int entrans = 5000000000
+                 int total = balans + entrans
+    }
+}

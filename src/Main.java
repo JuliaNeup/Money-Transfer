33cf24df44371329faeadcf03767d68b7e3333cb
Yulia <seminajlj@rambler.ru>
2021-03-24 22:21:11 +0300

    Исправление ошибок

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+         int balans = 2_000_000_000;
+         int entrans = 500_000_000 ;
+         int total = balans + entrans;
+                System.out.println(total);
+    }
+}
